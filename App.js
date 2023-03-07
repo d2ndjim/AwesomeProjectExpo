65ac1627e4764d1ff5f119ba778f62bf62a102d9
Lekan <jimoridwanlekan@gmail.com>
2023-03-07 19:01:47 +0100

    Add styling to RN component

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={[styles.container, styles.pink]}>
+        <Text>Hello, World!</Text>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
+  pink: {
+    backgroundColor: 'pink',
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flex: 1,
+  },
+  safeArea: {
+    flex: 2,
   },
 });
+
+export default App;
